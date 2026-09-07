775cea09051eea16a71399c9e9156675af39a614
Pranav-chitte <pranavchitte69@gmail.com>
2026-09-07 22:59:12 +0530

    Add files via upload

---
+++ insertion_sort.py
@@ -0,0 +1,14 @@
+def insertion_sort(arr):
+    
+    for i in range(len(arr)):
+        key = arr[i]
+        j=i-1
+
+        while j >=0 and arr[j]>key:
+            arr[j+1]=arr[j]
+            j-=1
+
+        arr[j+1]=key
+    return arr
+arr=[34,78,67,90]
+print(insertion_sort(arr))
